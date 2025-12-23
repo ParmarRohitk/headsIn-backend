9cb80f2f82d39240c0889d0b8a0aa7600695b56d
ParmarRohitk <parmarrohitk.bca@gmail.com>
2025-12-24 00:38:09 +0530

    Fix Render deployment: adding root entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * Render sometimes defaults to running 'node index.js' if not explicitly configured.
+ * This file acts as a proxy to the actual entry point in src/server.js.
+ */
+require("./src/server.js");
